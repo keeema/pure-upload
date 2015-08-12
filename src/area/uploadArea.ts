a741fb812fd2315272fd91169f08713a4dd93478
marekhoryna <marek.horyna@outlook.com>
2015-08-12 13:21:08 +0200

    Browser compatibility, directory upload added
    Solved some issues with browser compatibility, added directory uploading
    [Chrome only]

--- src/area/uploadArea.ts
+++ src/area/uploadArea.ts
@@ -23,6 +23,17 @@ class UploadArea {
         this.uploader.queue.addFiles(uploadFiles);
     }
 
+    private putFileToQueue(file: File): void {
+        let uploadFile: IUploadFile;
+        uploadFile = <IUploadFile>file;
+        uploadFile.progress = 0;
+        uploadFile.start = () => {
+            this.uploadCore.upload([file]);
+            uploadFile.start = () => { };
+        };
+        this.uploader.queue.addFiles([uploadFile]);
+    }
+
     private setupHiddenInput(): void {
         this.fileInput = document.createElement("input");
         this.fileInput.setAttribute("type", "file");
@@ -49,19 +60,6 @@ class UploadArea {
             var onDrop = (e) => this.onDrop(e);
             this.targetElement.addEventListener("drop", onDrop);
             this.unregisterOnDrop = () => this.targetElement.removeEventListener("drop", onDrop);
-
-            // this.targetElement.addEventListener("dragenter", (e) => {
-            //     console.log("dragenter");
-            //     console.log(e);
-            // });
-            // this.targetElement.addEventListener("dragstart", (e) => {
-            //     console.log("dragstart");
-            //     console.log(e);
-            // });
-            // this.targetElement.addEventListener("dragend", (e) => {
-            //     console.log("dragend");
-            //     console.log(e);
-            // });
         }
         // attach to body
         document.body.appendChild(this.fileInput);
@@ -81,15 +79,19 @@ class UploadArea {
     }
 
     private onDrop(e: DragEvent): void {
+        this.stopEventPropagation(e);
         if (!e.dataTransfer) {
             return;
         }
         var files = e.dataTransfer.files;
         if (files.length) {
-            var items = e.dataTransfer.files;
-            this.putFilesToQueue(items);
+            var items = e.dataTransfer.items;
+            if (items && items.length && ((<any>items[0]).webkitGetAsEntry != null)) {
+                this.addFilesFromItems(items);
+            } else {
+                this.handleFiles(files);
+            }
         }
-        this.stopEventPropagation(e);
     }
 
     private onClick(): void {
@@ -97,10 +99,60 @@ class UploadArea {
         this.fileInput.click();
     }
 
+    private addFilesFromItems(items: FileList): void {
+        var entry;
+        for (var i = 0; i < items.length; i++) {
+            let item: FileExt = <FileExt>items[i];
+            if ((item.webkitGetAsEntry) && (entry = item.webkitGetAsEntry())) {
+                if (entry.isFile) {
+                    this.putFileToQueue(item.getAsFile());
+                } else if (entry.isDirectory) {
+                    this.processDirectory(entry, entry.name);
+                }
+            } else if (item.getAsFile) {
+                if ((item.kind == null) || item.kind === "file") {
+                    this.putFileToQueue(item.getAsFile());
+                }
+            }
+        }
+    }
+
+    private processDirectory(directory: any, path: string): void {
+        var dirReader = directory.createReader();
+        var _class = this;
+        var entryReader = (entries: FileExt[]) => {
+            for (var i = 0; i < entries.length; i++) {
+                var entry = entries[i];
+                if (entry.isFile) {
+                    entry.file((file) => {
+                        if (file.name.substring(0, 1) === '.') {
+                            return;
+                        }
+                        file.fullPath = "" + path + "/" + file.name;
+                        _class.putFileToQueue(file);
+                    });
+                } else if (entry.isDirectory) {
+                    _class.processDirectory(entry, "" + path + "/" + entry.name)
+                }
+            }
+        };
+        return dirReader.readEntries(entryReader, function(error) {
+            return typeof console !== "undefined" && console !== null ? typeof console.log === "function" ? console.log(error) : void 0 : void 0;
+        });
+    }
+
+    private handleFiles(files: FileList): void {
+        for (var i = 0; i < files.length; i++) {
+            this.putFileToQueue(files[i]);
+        }
+    }
+
     private stopEventPropagation(e) {
         e.stopPropagation();
         if (e.preventDefault) {
             e.preventDefault();
+        } else {
+            return e.returnValue = false;
         }
     }
 
